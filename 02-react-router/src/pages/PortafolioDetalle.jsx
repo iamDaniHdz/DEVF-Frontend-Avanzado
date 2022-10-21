81aa5b9d625db58c142fd2bc09d01bafff3afa7d
Daniel Hernandez <daniel244@live.com.mx>
2022-10-21 12:00:09 -0500

    Enlaces con useNavigate con JS

--- 02-react-router/src/pages/PortafolioDetalle.jsx
+++ 02-react-router/src/pages/PortafolioDetalle.jsx
@@ -1,17 +1,28 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 const PortafolioDetalle = () => {
   const proyectos = [
     { id: 1, nombre: 'Pinterest', desc: 'Un clon de pinterest con HTML y CSS' },
-    { id: 2, nombre: 'Cajero', desc: 'Simulador de cajero autómatico con HTML, CSS y JS' },
+    { id: 2, nombre: 'Cajero', desc: 'Simulador de cajero automático con HTML, CSS y JS' },
     { id: 3, nombre: 'Pokédex', desc: 'Consumir la PokéAPI y mostrar un listado de Pokémons con HTML, CSS, y JS ' }
   ]
-  const { pid } = useParams()
+  const { pid } = useParams() // Tiene el número de proyecto que le mando en la ruta
+  const navigate = useNavigate() // Cambiar la ruta programaticamente
+
   return (
     <>
       <h3>ID: {proyectos[pid - 1].id}</h3>
       <h3>Nombre: {proyectos[pid - 1].nombre}</h3>
       <p>Descripción: {proyectos[pid - 1].desc}</p>
+
+      <button onClick={() => {
+        navigate('/portafolio')
+        // Si quiero ir a la página anterior del historial: navigate(-1)
+        // Si quiero ir a la página siguiente del historial: navigate(1)
+      }}
+      >
+        Nos vamos al portafolio
+      </button>
     </>
   )
 }
