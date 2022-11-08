80f8403a7185b6118cc2b453bc78a31f7b061b2c
Daniel Hernandez <daniel244@live.com.mx>
2022-11-07 22:24:34 -0600

    Estilos del Signup, y agregando assets

--- Ecommerce/src/pages/Signup.jsx
+++ Ecommerce/src/pages/Signup.jsx
@@ -1,4 +1,3 @@
-import logo from '@/assets/react.svg'
 import useForm from '@/hooks/useForm'
 import { useNavigate } from 'react-router-dom'
 import { registerUserServices } from '@/services/userServices'
@@ -28,75 +27,83 @@ const Signup = () => {
   })
 
   return (
-    <main className='form-signin w-100 m-auto'>
+    <main>
       <form onSubmit={handleSubmit}>
-        <img className='mb-4' src={logo} alt='' width='72' height='57' />
-        <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
-        <div className='form-floating'>
-          <input
-            type='text'
-            className='form-control'
-            id='first_name'
-            name='first_name'
-            placeholder='John'
-            value={input.first_name}
-            onChange={handleInputChange}
-          />
-          <label htmlFor='first_name'>First Name</label>
-        </div>
+        <div className='d-flex align-items-center justify-content-evenly container p-3'>
+          <div className='bg-light d-flex flex-column p-4 rounded w-25'>
+            <h1 className='h3 mb-3 fw-normal text-center'>Regístrate</h1>
+            <div className='form-floating m-1'>
+              <input
+                type='text'
+                className='form-control'
+                id='first_name'
+                name='first_name'
+                placeholder='John'
+                value={input.first_name}
+                onChange={handleInputChange}
+              />
+              <label htmlFor='first_name'>Nombre</label>
+            </div>
 
-        <div className='form-floating'>
-          <input
-            type='text'
-            className='form-control'
-            id='last_name'
-            name='last_name'
-            placeholder='Doe'
-            value={input.last_name}
-            onChange={handleInputChange}
-          />
-          <label htmlFor='last_name'>Last Name</label>
-        </div>
+            <div className='form-floating m-1'>
+              <input
+                type='text'
+                className='form-control'
+                id='last_name'
+                name='last_name'
+                placeholder='Doe'
+                value={input.last_name}
+                onChange={handleInputChange}
+              />
+              <label htmlFor='last_name'>Apellido</label>
+            </div>
 
-        <div className='form-floating'>
-          <select className='form-select' id='gender' name='gender' value={input.gender} onChange={handleInputChange}>
-            <option value=''>Choose...</option>
-            <option value='M'>Male</option>
-            <option value='F'>Female</option>
-          </select>
-          <label htmlFor='gender'>Gender</label>
-        </div>
+            <div className='form-floating m-1'>
+              <select className='form-select' id='gender' name='gender' value={input.gender} onChange={handleInputChange}>
+                <option value=''>Escoge...</option>
+                <option value='M'>Hombre</option>
+                <option value='F'>Mujer</option>
+              </select>
+              <label htmlFor='gender'>Género</label>
+            </div>
 
-        <div className='form-floating'>
-          <input
-            type='email'
-            className='form-control'
-            id='email'
-            name='email'
-            placeholder='name@example.com'
-            value={input.email}
-            onChange={handleInputChange}
-          />
-          <label htmlFor='email'>Email address</label>
-        </div>
+            <div className='form-floating m-1'>
+              <input
+                type='email'
+                className='form-control'
+                id='email'
+                name='email'
+                placeholder='name@example.com'
+                value={input.email}
+                onChange={handleInputChange}
+              />
+              <label htmlFor='email'>Correo electrónico</label>
+            </div>
 
-        <div className='form-floating'>
-          <input
-            type='password'
-            className='form-control'
-            id='password'
-            name='password'
-            placeholder='Password'
-            value={input.password}
-            onChange={handleInputChange}
-          />
-          <label htmlFor='password'>Password</label>
-        </div>
+            <div className='form-floating m-1'>
+              <input
+                type='password'
+                className='form-control'
+                id='password'
+                name='password'
+                placeholder='Password'
+                value={input.password}
+                onChange={handleInputChange}
+              />
+              <label htmlFor='password'>Contraseña</label>
+            </div>
 
-        <button className='w-100 btn btn-lg btn-primary' type='submit'>
-          Sign Up
-        </button>
-        <p className='mt-5 mb-3 text-muted'>© 2017–2022</p>
+            <button className='btn btn-lg btn-dark m-1' type='submit'>
+              Registrarse
+            </button>
+
+            <p className='mt-3'>¿Ya tienes una cuenta? <a href='' className='text-warning'>Iniciar sesión</a></p>
+
+          </div>
+          <div className=''>
+            <img className='' src='src/assets/register.svg' alt='' width='350rem' />
+          </div>
+        </div>
       </form>
     </main>
   )
